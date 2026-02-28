10e03a451f9a5a75d5835712e152a2ca54d6afc6
sak <33581588+firament@users.noreply.github.com>
2026-02-28 16:13:35 +0530

    Enable healthcheck by default

--- setup-fdd.sh
+++ setup-fdd.sh
@@ -24,7 +24,7 @@ echo "Writing log to files in ${SETUPS_LOG_LOCN}";
 echo;
 
 ## Individual steps that are to be executed
-    # HealthCheck        2>&1 | tee -a ${SETUPS_LOG_LOCN}/00-HealthCheck-$(date +"%Y%m%d-%s").log;
+    HealthCheck        2>&1 | tee -a ${SETUPS_LOG_LOCN}/00-HealthCheck-$(date +"%Y%m%d-%s").log;
     # Init               2>&1 | tee -a ${SETUPS_LOG_LOCN}/10-Init-$(date +"%Y%m%d-%s").log;
     # InstallCoreApps    2>&1 | tee -a ${SETUPS_LOG_LOCN}/20-InstallCoreApps-$(date +"%Y%m%d-%s").log;
     # SetupDevApps       2>&1 | tee -a ${SETUPS_LOG_LOCN}/30-SetupDevApps-$(date +"%Y%m%d-%s").log;
